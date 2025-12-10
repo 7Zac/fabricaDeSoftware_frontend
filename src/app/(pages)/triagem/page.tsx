9eca179b3266d1f726df9f70b45249c9f87983d9
GuilhermeMarqus <gjanuario403@gmail.com>
2025-12-10 02:48:27 -0300

    fix: triagem

--- src/app/(pages)/triagem/page.tsx
+++ src/app/(pages)/triagem/page.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import AddPatientPage from "../addPatient/page";
 import HeaderGuiche from "@/app/components/HeaderGuiche";
+import HeaderTriagem from "@/app/components/HeaderTriagem";
 
 interface newPatient {
   name: string;
@@ -20,16 +21,16 @@ interface newPatient {
 }
 
 const TriagemPage = () => {
-  const [activeTab, setActiveTab] = useState<"G" | "S">("G");
+  const [activeTab, setActiveTab] = useState<"T" | "S">("T");
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 transition-all duration-500 overflow-hidden">
       {/* Cabeçalho */}
-     <HeaderGuiche activeTab={activeTab} setActiveTab={setActiveTab} />
+     <HeaderTriagem activeTab={activeTab} setActiveTab={setActiveTab} />
       {/* Conteúdo Principal */}
       <main className="flex-1 flex p-4 space-x-4 relative overflow-hidden">
         <AnimatePresence mode="wait">
-          {activeTab === "G" ? (
+          {activeTab === "T" ? (
             <motion.div
               key="guiche"
               initial={{ opacity: 0, x: -30 }}
@@ -40,7 +41,6 @@ const TriagemPage = () => {
             >
               {/* Seção FILA */}
               <div className="w-1/3 bg-white rounded-lg shadow-md p-4">
-                <h2 className="text-xl font-semibold mb-4 text-gray-700">FILA</h2>
                 <QueueList />
               </div>
 
@@ -73,7 +73,7 @@ const TriagemPage = () => {
 
       {/* Botão Flutuante com transição */}
       <AnimatePresence mode="wait">
-        {activeTab === "G" ? (
+        {activeTab === "T" ? (
           <motion.div
             key="btnG"
             initial={{ opacity: 0, y: 20 }}
