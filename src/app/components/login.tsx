759a4ce48f13dac260a6fad2602ab36997940ae6
GuilhermeMarqus <gjanuario403@gmail.com>
2025-12-10 02:48:34 -0300

    fix: login

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -13,15 +13,9 @@ import { Label } from "@/components/ui/label";
 
 const Login = () => {
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
+    <Card className="w-full max-w-sm gap-6">
+      <CardHeader className="flex justify-center">
         <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
       </CardHeader>
       <CardContent>
         <form>
@@ -38,12 +32,6 @@ const Login = () => {
             <div className="grid gap-2">
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
               </div>
               <Input id="password" type="password" required />
             </div>
@@ -53,9 +41,6 @@ const Login = () => {
       <CardFooter className="flex-col gap-2">
         <Button type="submit" className="w-full">
           Login
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Google
         </Button>
       </CardFooter>
     </Card>
