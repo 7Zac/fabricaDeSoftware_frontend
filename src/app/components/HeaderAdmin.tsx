76a3d1e2004602151a99f4ba9bf34052087b674b
7Zac <isaac.lopesalencar@gmail.com>
2025-12-11 00:06:09 -0300

    chore: add button to route /addPatient in admin/home, styled button exit, add FileSymlink and UserPlus icons lucide-react

--- src/app/components/HeaderAdmin.tsx
+++ src/app/components/HeaderAdmin.tsx
@@ -3,30 +3,50 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { FileSymlink, UserPlus } from "lucide-react";
 
 const HeaderAdmin = ({}) => {
   const router = useRouter();
   return (
     <header className="flex items-center justify-between p-4 bg-white shadow-md">
       <div className="flex items-center space-x-2">
-        <Image alt="Logomarca" src="/Logo.svg" width="50" height="50"
-        className="cursor-pointer"
-        onClick={() => router.push("/admin/home")} />
-        <span className="text-lg font-semibold text-teal-600 cursor-pointer"
-        onClick={() => router.push("/admin/home")}>
+        <Image
+          alt="Logomarca"
+          src="/Logo.svg"
+          width="50"
+          height="50"
+          className="cursor-pointer"
+          onClick={() => router.push("/admin/home")}
+        />
+        <span
+          className="text-lg font-semibold text-teal-600 cursor-pointer"
+          onClick={() => router.push("/admin/home")}
+        >
           CLINICA SAÚDE
         </span>
       </div>
 
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center gap-20">
         {/* Botão Sair */}
-        <Button className="bg-teal-500 hover:bg-teal-600 text-white " 
-        onClick={() => router.push("/admin/createAnuncioPage")}>
-              Adicionar Anúncio
-        </Button>
+        <div className="flex items-center space-x-4">
+          <Button
+            className="bg-teal-500 hover:bg-teal-600 text-white "
+            onClick={() => router.push("/admin/createAnuncioPage")}
+          >
+            <FileSymlink />
+            Adicionar Anúncio
+          </Button>
+          <Button
+            className="bg-teal-500 hover:bg-teal-600 text-white "
+            onClick={() => router.push("/addPatient")}
+          >
+            <UserPlus />
+            Adicionar Paciente
+          </Button>
+        </div>
         <Button
           variant="outline"
-          className="text-teal-600 border-teal-600 hover:bg-teal-50"
+          className="text-teal-600 hover:border-red-700 hover:text-red-700 hover:bg-red-50"
         >
           Sair
         </Button>
