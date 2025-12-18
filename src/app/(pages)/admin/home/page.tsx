137d27a2bfdb08b9f8c422a9cc6d8978b113be6f
7Zac <isaac.lopesalencar@gmail.com>
2025-12-18 17:33:58 -0300

    chore: styled admin home page

--- src/app/(pages)/admin/home/page.tsx
+++ src/app/(pages)/admin/home/page.tsx
@@ -11,12 +11,12 @@ const AdminHomePage = () => {
  
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100 transition-all duration-500 overflow-hidden">
+    <div className="flex flex-col min-h-screen bg-gray-100 transition-all duration-500">
       {/* Cabeçalho */}
       <HeaderAdmin />
 
       {/* Conteúdo Principal */}
-      <main className="flex-1 flex p-4 space-x-4 relative overflow-hidden">
+      <main className="flex-1 flex flex-wrap p-4 gap-4 relative">
         {/* Seção FILA */}
         <div className="w-1/3 bg-white rounded-lg shadow-md p-4">
           <h2 className="text-xl font-semibold mb-4 text-gray-700">USUÁRIOS</h2>
